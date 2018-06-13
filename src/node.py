c977df53cf3241f6131672fd7d333aa5570ed76f
Eddie <ehwa009@aucklanduni.ac.nz>
2018-06-13 22:34:01 +1200

    integratio with pollySpeech

--- src/node.py
+++ src/node.py
@@ -12,6 +12,9 @@ import boto3
 from mind_msgs.msg import Reply, RaisingEvents
 from mind_msgs.srv import ReloadWithResult, ReadData, WriteData
 
+import actionlib
+from polly_speech.msg import SpeechAction, SpeechGoal
+
 class LexHandler: 
     def __init__(self):
         rospy.init_node('lex_intent_handler', anonymous=False)
@@ -40,6 +43,15 @@ class LexHandler:
         rospy.loginfo('[%s] Initialzed'%rospy.get_name())
         rospy.spin()
 
+    def func_done(self, state, result):
+        pass
+
+    def func_feedback(self, feedback):
+        rospy.loginfo(feedback.is_speaking)
+
+    def func_active(self):
+        pass
+    
     def handle_raise_events(self, msg):
         if msg.recognized_word != '':
             response = self.client.post_content(
@@ -50,12 +62,22 @@ class LexHandler:
                 sessionAttributes = self.session_attributes,
                 accept = 'text/plain; charset=utf-8',
                 inputStream = msg.recognized_word)
-
+            
+            # msg for SHIR project
             reply_msg = Reply()
             reply_msg.header.stamp = rospy.Time.now()
             reply_msg.reply = response.get('message')
-            
             self.pub_reply.publish(reply_msg)
+            
+            # msg for Polly speech
+            client = actionlib.SimpleActionClient('internal_speech', SpeechAction)
+            client.wait_for_server()
+
+            goal = SpeechGoal()
+            goal.text = response.get('message')
+            client.send_goal(goal, done_cb=self.func_done, feedback_cb=self.func_feedback, active_cb=self.func_active)
+            client.wait_for_result()
+            exit(1)
         else:
             rospy.loginfo('getting signal from guide bot')
 
